n-000000/sveltia-cms

commit 0c071ed31aba0c494b555559fb48c4a54963edee

Enable all strict type checks

src/lib/components/contents/details/widgets/file/file-editor.svelte
@@ -53,23 +53,23 @@
   export let invalid = false;
 
   /**
-   * @type {Asset}
+   * @type {Asset | undefined}
    */
   let asset;
   /**
-   * @type {File}
+   * @type {File | undefined}
    */
   let file;
   /**
-   * @type {string}
+   * @type {string | undefined}
    */
   let url;
   /**
-   * @type {string}
+   * @type {string | undefined}
    */
   let src;
   /**
-   * @type {string}
+   * @type {string | undefined}
    */
   let credit;
   let showSelectAssetsDialog = false;
@@ -101,14 +101,19 @@
     if (file) {
       // Check the max file size
       // @see https://decapcms.org/docs/widgets/#image
-      if (isImageWidget && Number.isInteger(maxFileSize) && file.size > maxFileSize) {
+      if (
+        isImageWidget &&
+        maxFileSize !== undefined &&
+        Number.isInteger(maxFileSize) &&
+        file.size > maxFileSize
+      ) {
         showSizeLimitDialog = true;
       } else {
         // Use the `data:` URL temporarily, and replace it later; avoid `blob:` here because it will
         // be unavailable event after Vite HMR
         currentValue = await getDataURL(file);
         // Cache the file itself for later upload
-        $entryDraft.files[locale][keyPath] = file;
+        /** @type {EntryDraft} */ ($entryDraft).files[locale][keyPath] = file;
       }
     }
 
@@ -124,7 +129,7 @@
 
   $: (async () => {
     src =
-      isImageWidget && currentValue
+      isImageWidget && currentValue && $entryDraft?.originalEntry
         ? await getMediaFieldURL(currentValue, $entryDraft.originalEntry)
         : undefined;
   })();
@@ -195,7 +200,9 @@
 />
 
 <AlertDialog bind:open={showSizeLimitDialog} title={$_('assets_dialog.large_file.title')}>
-  {$_('assets_dialog.large_file.description', { values: { size: formatSize(maxFileSize) } })}
+  {$_('assets_dialog.large_file.description', {
+    values: { size: formatSize(/** @type {number} */ (maxFileSize)) },
+  })}
 </AlertDialog>
 
 <ConfirmationDialog

src/lib/components/contents/details/widgets/file/file-preview.svelte
@@ -29,7 +29,7 @@
   export let currentValue;
 
   /**
-   * @type {string}
+   * @type {string | undefined}
    */
   let src;
 
@@ -38,7 +38,7 @@
 
   $: (async () => {
     src =
-      isImageWidget && currentValue
+      isImageWidget && currentValue && $entryDraft?.originalEntry
         ? await getMediaFieldURL(currentValue, $entryDraft.originalEntry)
         : undefined;
   })();

src/lib/services/assets/index.js
@@ -88,7 +88,12 @@ export const getAssetByPath = (savedPath, entry) => {
 
     const { collectionName, fileName, locales } = entry;
     const collection = getCollection(collectionName);
-    const collectionFile = fileName ? collection._fileMap[fileName] : undefined;
+
+    if (!collection) {
+      return undefined;
+    }
+
+    const collectionFile = fileName ? collection._fileMap?.[fileName] : undefined;
     const { defaultLocale } = (collectionFile ?? collection)._i18n;
     const locale = defaultLocale in locales ? defaultLocale : Object.keys(locales)[0];
     const { path: entryFilePath, content: entryContent } = locales[locale];
@@ -97,7 +102,7 @@ export const getAssetByPath = (savedPath, entry) => {
       return undefined;
     }
 
-    const [, entryFolder] = entryFilePath.match(/(.+?)(?:\/[^/]+)?$/);
+    const [, entryFolder] = entryFilePath.match(/(.+?)(?:\/[^/]+)?$/) ?? [];
     const resolvedPath = resolvePath(`${entryFolder}/${savedPath}`);
 
     return get(allAssets).find((asset) => asset.path === resolvedPath);
@@ -121,7 +126,7 @@ export const getAssetByPath = (savedPath, entry) => {
 
 /**
  * Get the public URL for the given asset.
- * @param {Asset} asset - Asset file, such as an image.
+ * @param {Asset | undefined} asset - Asset file, such as an image.
  * @param {object} [options] - Options.
  * @param {boolean} [options.pathOnly] - Whether to use the absolute path instead of the complete
  * URL.
@@ -140,7 +145,13 @@ export const getAssetURL = async (asset, { pathOnly = false } = {}) => {
   }
 
   if (!asset.url && (asset.file || asset.fetchURL) && !pathOnly) {
-    const url = URL.createObjectURL(asset.file || (await get(backend).fetchBlob(asset)));
+    const blob = asset.file ?? (await get(backend)?.fetchBlob?.(asset));
+
+    if (!blob) {
+      return undefined;
+    }
+
+    const url = URL.createObjectURL(blob);
 
     // Cache the URL
     allAssets.update((assets) => [
@@ -152,15 +163,16 @@ export const getAssetURL = async (asset, { pathOnly = false } = {}) => {
   }
 
   const { publicPath, entryRelative } =
-    get(allAssetFolders).find(({ collectionName }) => collectionName === asset.collectionName) ||
-    get(allAssetFolders).find(({ collectionName }) => collectionName === null);
+    get(allAssetFolders).find(({ collectionName }) => collectionName === asset.collectionName) ??
+    get(allAssetFolders).find(({ collectionName }) => collectionName === null) ??
+    {};
 
   if (entryRelative) {
     return isBlobURL ? asset.url : undefined;
   }
 
-  const baseURL = pathOnly ? '' : get(siteConfig).site_url ?? '';
-  const path = asset.path.replace(asset.folder, publicPath);
+  const baseURL = pathOnly ? '' : get(siteConfig)?.site_url ?? '';
+  const path = asset.path.replace(asset.folder, publicPath ?? '');
 
   return `${baseURL}${path}`;
 };
@@ -203,7 +215,7 @@ export const getAssetDetails = async (asset) => {
     displayURL: url,
     dimensions,
     duration,
-    usedEntries: await getEntriesByAssetURL(url),
+    usedEntries: url ? await getEntriesByAssetURL(url) : [],
   };
 };
 

src/lib/services/backends/index.js
@@ -18,10 +18,10 @@ export const allBackendServices = {
 export const backendName = writable();
 
 /**
- * @type {import('svelte/store').Readable<BackendService>}
+ * @type {import('svelte/store').Readable<BackendService | undefined>}
  */
 export const backend = derived([backendName], ([name], set) => {
-  const service = allBackendServices[name];
+  const service = name ? allBackendServices[name] : undefined;
 
   service?.init();
   set(service);

src/lib/services/contents/index.js
@@ -36,7 +36,7 @@ export const selectedEntries = writable([]);
  * @returns {Collection | undefined} Collection, including some extra, normalized properties.
  */
 export const getCollection = (name) => {
-  const collection = get(siteConfig).collections.find((c) => c.name === name);
+  const collection = get(siteConfig)?.collections.find((c) => c.name === name);
 
   if (!collection) {
     return undefined;
@@ -61,7 +61,7 @@ export const getCollection = (name) => {
  * Get a file collection entry.
  * @param {string} collectionName - Collection name.
  * @param {string} fileName - File name.
- * @returns {Entry} File.
+ * @returns {Entry | undefined} File.
  * @see https://decapcms.org/docs/collection-types/#file-collections
  */
 export const getFile = (collectionName, fileName) =>
@@ -100,7 +100,8 @@ export const getEntriesByCollection = (collectionName) => {
  * @returns {Promise<Entry[]>} Entries.
  */
 export const getEntriesByAssetURL = async (url) => {
-  const path = url.replace(get(siteConfig).site_url, '');
+  const siteURL = get(siteConfig)?.site_url;
+  const path = siteURL ? url.replace(siteURL, '') : url;
   const entries = get(allEntries);
 
   const results = await Promise.all(
@@ -115,7 +116,7 @@ export const getEntriesByAssetURL = async (url) => {
             Object.entries(valueMap).map(async ([keyPath, value]) => {
               const field = getFieldConfig({ collectionName, fileName, valueMap, keyPath });
 
-              if (!field || !['image', 'file'].includes(field.widget)) {
+              if (!field || !['image', 'file'].includes(field.widget ?? 'string')) {
                 return false;
               }
 

commit aa7b4df1bb959dfb12c778df10741f5f5957ad0d

Fix image preview issue on entry draft
Regressed in 0c071ed31aba0c494b555559fb48c4a54963edee

src/lib/components/contents/details/widgets/file/file-editor.svelte
@@ -129,8 +129,8 @@
 
   $: (async () => {
     src =
-      isImageWidget && currentValue && $entryDraft?.originalEntry
-        ? await getMediaFieldURL(currentValue, $entryDraft.originalEntry)
+      isImageWidget && currentValue
+        ? await getMediaFieldURL(currentValue, $entryDraft?.originalEntry)
         : undefined;
   })();
 </script>

src/lib/components/contents/details/widgets/file/file-preview.svelte
@@ -38,8 +38,8 @@
 
   $: (async () => {
     src =
-      isImageWidget && currentValue && $entryDraft?.originalEntry
-        ? await getMediaFieldURL(currentValue, $entryDraft.originalEntry)
+      isImageWidget && currentValue
+        ? await getMediaFieldURL(currentValue, $entryDraft?.originalEntry)
         : undefined;
   })();
 </script>

src/lib/services/assets/index.js
@@ -75,7 +75,8 @@ export const getAssetKind = (name) =>
 /**
  * Get an asset by a public path typically stored as an image field value.
  * @param {string} savedPath - Saved absolute path or relative path.
- * @param {Entry} entry - Associated entry to be used to help locale an asset from a relative path.
+ * @param {Entry} [entry] - Associated entry to be used to help locale an asset from a relative
+ * path. Can be `undefined` when editing a draft.
  * @returns {(Asset | undefined)} Corresponding asset.
  */
 export const getAssetByPath = (savedPath, entry) => {
@@ -181,7 +182,8 @@ export const getAssetURL = async (asset, { pathOnly = false } = {}) => {
  * Get the public URL from the given image/file entry field value.
  * @param {string} value - Saved field value. It can be an absolute path, entry-relative path, or a
  * complete/external URL.
- * @param {Entry} entry - Associated entry.
+ * @param {Entry} [entry] - Associated entry to be used to help locale an asset from a relative
+ * path. Can be `undefined` when editing a draft.
  * @returns {Promise<(string | undefined)>} URL that can be displayed in the app UI.
  */
 export const getMediaFieldURL = async (value, entry) => {