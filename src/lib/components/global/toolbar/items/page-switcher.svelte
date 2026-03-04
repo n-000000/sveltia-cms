n-000000/sveltia-cms

commit 7e0d821bd5dd3d269dc462e59751f931b25c9620

Refactor page-switcher to dynamically build pages list

src/lib/components/global/toolbar/items/page-switcher.svelte
@@ -4,44 +4,57 @@
 
   import { goto, selectedPageName } from '$lib/services/app/navigation';
   import { selectedAssetFolder } from '$lib/services/assets/folders';
+  import { backendName } from '$lib/services/backends';
+  import { cmsConfig } from '$lib/services/config';
   import { isSmallScreen } from '$lib/services/user/env';
 
-  const pages = $derived([
-    {
-      key: 'collections',
-      label: $_('contents'),
-      icon: 'library_books',
-      link: '/collections',
-    },
-    {
-      key: 'assets',
-      label: $_('assets'),
-      icon: 'photo_library',
-      link: $isSmallScreen ? '/assets' : `/assets/${$selectedAssetFolder?.internalPath ?? '-/all'}`,
-    },
-    // {
-    //   key: 'workflow',
-    //   label: $_('editorial_workflow'),
-    //   icon: 'rebase_edit',
-    //   link: '/workflow',
-    // },
-    // {
-    //   key: 'config',
-    //   label: $_('cms_config'),
-    //   icon: 'settings',
-    //   link: '/config',
-    // },
-    ...($isSmallScreen
-      ? [
-          {
-            key: 'menu',
-            label: $_('menu'),
-            icon: 'menu',
-            link: '/menu',
-          },
-        ]
-      : []),
-  ]);
+  const pages = $derived.by(() => {
+    const _pages = [
+      {
+        key: 'collections',
+        label: $_('contents'),
+        icon: 'library_books',
+        link: '/collections',
+      },
+      {
+        key: 'assets',
+        label: $_('assets'),
+        icon: 'photo_library',
+        link: $isSmallScreen
+          ? '/assets'
+          : `/assets/${$selectedAssetFolder?.internalPath ?? '-/all'}`,
+      },
+    ];
+
+    if ($cmsConfig?.publish_mode === 'editorial_workflow') {
+      // _pages.push({
+      //   key: 'workflow',
+      //   label: $_('editorial_workflow'),
+      //   icon: 'rebase_edit',
+      //   link: '/workflow',
+      // });
+    }
+
+    if ($backendName === 'local') {
+      // _pages.push({
+      //   key: 'config',
+      //   label: $_('cms_config'),
+      //   icon: 'settings',
+      //   link: '/config',
+      // });
+    }
+
+    if ($isSmallScreen) {
+      _pages.push({
+        key: 'settings',
+        label: $_('settings'),
+        icon: 'settings',
+        link: '/settings',
+      });
+    }
+
+    return _pages;
+  });
 </script>
 
 <div role="none" class="wrapper">

commit 267f215745add9b20e36e1bc39861807e4838b2d

Fix navigation item on mobile
Regressed in 7e0d821bd5dd3d269dc462e59751f931b25c9620

src/lib/components/global/toolbar/items/page-switcher.svelte
@@ -51,10 +51,10 @@
 
     if ($isSmallScreen) {
       _pages.push({
-        key: 'settings',
-        label: $_('settings'),
-        icon: 'settings',
-        link: '/settings',
+        key: 'menu',
+        label: $_('menu'),
+        icon: 'menu',
+        link: '/menu',
       });
     }
 